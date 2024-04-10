3c7a78372e56e5f1b4dfb08f8162a12e1e67defb
pmrizzo96 <164394790+pmrizzo96@users.noreply.github.com>
2024-04-10 22:42:43 +0200

    Add files via upload

---
+++ include/OpenAI_configuration.php
@@ -0,0 +1,7 @@
+<?php
+class AIConfig {
+public $apiKey = "...............";
+public $assistant_id = '.......................';
+public $openai_organization = ".................";
+}
+?>
---
+++ include/db_configuration.php
@@ -0,0 +1,13 @@
+<?php
+
+class DBConfig {
+    public $dbtype = "mysqli";
+    public $host = 'localhost';
+    public $user = 'xxxx';
+    public $password = 'xxxx';
+    public $db = 'xxxx';
+    public $dbprefix = "xxxx_";
+}
+
+
+?>
